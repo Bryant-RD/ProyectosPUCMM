46b337bcdf0c8c666ee771b5fe03c4ea702e0424
Bryant Reynoso <Bryant Reynoso@DESKTOP-GVCQ3QP>
2021-11-18 16:14:33 -0400

    arreglando

--- ProyectoFinal/src/backend/Comision.java
+++ ProyectoFinal/src/backend/Comision.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class Comision {
 
 	private String nombre;
-	//private Jurado presidente;
-	//private ArrayList<Jurado> jurados;
-	//private ArrayList<Trabajo> trabajos;
+//	private Jurado presidente;
+//	private ArrayList<Jurado> jurados;
+	private ArrayList<Trabajo> trabajos;
 	private String areaConocimiento;
 	
 	
@@ -17,24 +17,24 @@ public class Comision {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-/*	public Jurado getPresidente() {
-		return presidente;
-	}
-	public void setPresidente(Jurado presidente) {
-		this.presidente = presidente;
-	}
-	public ArrayList<Jurado> getJurados() {
-		return jurados;
-	}
-	public void setJurados(ArrayList<Jurado> jurados) {
-		this.jurados = jurados;
-	}
+//	public Jurado getPresidente() {
+//		return presidente;
+//	}
+//	public void setPresidente(Jurado presidente) {
+//		this.presidente = presidente;
+//	}
+//	public ArrayList<Jurado> getJurados() {
+//		return jurados;
+//	}
+//	public void setJurados(ArrayList<Jurado> jurados) {
+//		this.jurados = jurados;
+//	}
 	public ArrayList<Trabajo> getTrabajos() {
 		return trabajos;
 	}
 	public void setTrabajos(ArrayList<Trabajo> trabajos) {
 		this.trabajos = trabajos;
-	}*/
+	}
 	public String getAreaConocimiento() {
 		return areaConocimiento;
 	}
