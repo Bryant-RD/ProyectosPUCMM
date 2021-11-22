5a1cd7fb5151c3bab521677f8f0462eb4c8b16dd
Bryant_Reynoso <briant04.rt@gmail.com>
2021-11-22 17:14:39 -0400

    asdsad

---
+++ ProyectoFinal/src/backend/Actividad.java
@@ -0,0 +1,12 @@
+package backend;
+
+public class Actividad extends Evento{
+	
+	private String tipo;
+	private String duracion;
+
+	public Actividad(String codigo, String nombre, String tema, String fecha, Participante ganador, String local) {
+		super(codigo, nombre, tema, fecha, ganador, local);
+	}
+
+}
--- ProyectoFinal/src/backend/PUCMM.java
+++ ProyectoFinal/src/backend/PUCMM.java
@@ -1,6 +1,44 @@
 package backend;
 
+import java.util.ArrayList;
+
 public class PUCMM {
 
-	/*Hola maria*/
+	private ArrayList<Persona> personas;
+	private ArrayList<Trabajo> trabajos;
+	private ArrayList<Comision> comisiones;
+	private ArrayList<Evento> eventos;
+	private ArrayList<Recursos> recursos;
+	private ArrayList <Actividad> actividades;
+	public static PUCMM pucmm = null;
+	
+	public PUCMM() {
+		personas = new ArrayList<>();
+		trabajos = new ArrayList<>();
+		comisiones = new ArrayList<>();
+		eventos = new ArrayList<>();
+		recursos = new ArrayList<>();
+		actividades = new ArrayList<>();
+		
+	}
+	
+	public static PUCMM getInstance() {
+		
+		if(pucmm == null) {
+			pucmm = new PUCMM();
+		}
+		return pucmm;
+	}
+	
+	public void crearEvento(Evento evento) {
+		/*Chequear si hay recursos
+		 * Tambien si hay jurados disponibles para la comision*/
+		
+		
+		
+	}
+	
+	
+	
+	
 }
